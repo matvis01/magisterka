91e080d9cd9de8782ca8f5a1c223d3f81cba7039
matvis01 <mati.siwy@op.pl>
2024-08-09 14:51:41 +0200

    Add contact form is a modal

--- solidjs-contact-manager/src/components/AddContact.tsx
+++ solidjs-contact-manager/src/components/AddContact.tsx
@@ -40,83 +40,92 @@ const AddContact = (props: AddContactProps) => {
     setEmail("");
     setPhone("");
     setImage("");
+
+    (document.getElementById("add_modal") as HTMLDialogElement)?.close();
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      class=" p-4 bg-neutral card rounded-md grid grid-cols-2 w-full gap-3 items-end"
-    >
-      <div class="form-control">
-        <label class="label" for="name">
-          <span class="label-text">Name</span>
-        </label>
-        <input
-          type="text"
-          id="name"
-          value={name()}
-          onInput={(e) => setName(e.currentTarget.value)}
-          class="input input-bordered w-full"
-          required
-        />
+    <dialog class="modal" id="add_modal">
+      <div class="modal-box">
+        <form
+          onSubmit={handleSubmit}
+          class=" p-4  card rounded-md grid grid-cols-2 w-full gap-3 items-end"
+        >
+          <div class="form-control">
+            <label class="label" for="name">
+              <span class="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={name()}
+              onInput={(e) => setName(e.currentTarget.value)}
+              class="input input-bordered w-full"
+              required
+            />
+          </div>
+          <div class="form-control">
+            <label class="label" for="surname">
+              <span class="label-text">Surname</span>
+            </label>
+            <input
+              type="text"
+              id="surname"
+              value={surname()}
+              onInput={(e) => setSurname(e.currentTarget.value)}
+              class="input input-bordered w-full"
+              required
+            />
+          </div>
+          <div class="form-control">
+            <label class="label" for="email">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email()}
+              onInput={(e) => setEmail(e.currentTarget.value)}
+              class="input input-bordered w-full"
+              required
+            />
+          </div>
+          <div class="form-control">
+            <label class="label" for="phone">
+              <span class="label-text">Phone</span>
+            </label>
+            <input
+              type="tel"
+              id="phone"
+              value={phone()}
+              onInput={(e) => setPhone(e.currentTarget.value)}
+              class="input input-bordered w-full"
+              required
+            />
+          </div>
+          <div class="form-control">
+            <label class="label" for="image">
+              <span class="label-text">Image URL (optional)</span>
+            </label>
+            <input
+              type="text"
+              id="image"
+              value={image()}
+              onInput={(e) => setImage(e.currentTarget.value)}
+              class="input input-bordered w-full"
+            />
+          </div>
+          <div class="form-control mt-6">
+            <button type="submit" class="btn btn-primary w-full">
+              Add Contact
+            </button>
+          </div>
+        </form>
       </div>
-      <div class="form-control">
-        <label class="label" for="surname">
-          <span class="label-text">Surname</span>
-        </label>
-        <input
-          type="text"
-          id="surname"
-          value={surname()}
-          onInput={(e) => setSurname(e.currentTarget.value)}
-          class="input input-bordered w-full"
-          required
-        />
-      </div>
-      <div class="form-control">
-        <label class="label" for="email">
-          <span class="label-text">Email</span>
-        </label>
-        <input
-          type="email"
-          id="email"
-          value={email()}
-          onInput={(e) => setEmail(e.currentTarget.value)}
-          class="input input-bordered w-full"
-          required
-        />
-      </div>
-      <div class="form-control">
-        <label class="label" for="phone">
-          <span class="label-text">Phone</span>
-        </label>
-        <input
-          type="tel"
-          id="phone"
-          value={phone()}
-          onInput={(e) => setPhone(e.currentTarget.value)}
-          class="input input-bordered w-full"
-          required
-        />
-      </div>
-      <div class="form-control">
-        <label class="label" for="image">
-          <span class="label-text">Image URL (optional)</span>
-        </label>
-        <input
-          type="text"
-          id="image"
-          value={image()}
-          onInput={(e) => setImage(e.currentTarget.value)}
-          class="input input-bordered w-full"
-        />
-      </div>
-      <div class="form-control mt-6">
-        <button type="submit" class="btn btn-primary w-full">
-          Add Contact
-        </button>
-      </div>
-    </form>
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
   );
 };
 
--- solidjs-contact-manager/src/routes/index.tsx
+++ solidjs-contact-manager/src/routes/index.tsx
@@ -47,19 +47,32 @@ export default function Home() {
     <main class="w-full h-full flex justify-center text-base-content ">
       <div class="w-full max-w-screen-xl flex flex-col items-start gap-10 p-10">
         <h1 class="text text-4xl font-bold">Contacts</h1>
-        <input
-          class="input input-lg input-primary input-bordered w-full lg:w-1/2 xl:w-96"
-          type="text"
-          placeholder="Search"
-          value={searchVal()}
-          onInput={(e) => filterContacts(e.currentTarget.value)}
-        />
+        <div class="flex gap-5">
+          <input
+            class="input input-lg input-primary input-bordered w-full lg:w-1/2 xl:w-96"
+            type="text"
+            placeholder="Search"
+            value={searchVal()}
+            onInput={(e) => filterContacts(e.currentTarget.value)}
+          />
+          <button
+            class="btn btn-primary btn-lg"
+            onClick={() =>
+              (
+                document.getElementById("add_modal") as HTMLDialogElement
+              )?.showModal()
+            }
+          >
+            add contact
+          </button>
+
+          <AddContact onAddContact={addContact} />
+        </div>
         <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 w-full justify-items-center items-center">
           {contacts().map((contact) => (
             <Contact {...contact} />
           ))}
         </div>
-        <AddContact onAddContact={addContact} />
       </div>
     </main>
   );
